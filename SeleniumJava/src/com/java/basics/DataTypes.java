055d86b3c0ea89c8c0f381a18b12adc583bd3afd
nareshbabumandula <naresh223@gmail.com>
2024-07-29 06:51:37 +0530

    29-Jul-2024: Naresh : My first java project

---
+++ SeleniumJava/src/com/java/basics/DataTypes.java
@@ -0,0 +1,27 @@
+package com.java.basics;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+	    // In Java we have 8 primitive data types		
+		byte b=123; // -128 to +127 --> 1 byte - 8 bits 
+	    short s=31900;
+		int a=10;
+		long l=73462;
+		float f=13.6f;
+		double d=536.464564;
+		boolean bFlag=true;
+		char c='t';
+		
+		System.out.println("byte value is : " + b);
+		System.out.println("short value is : " + s);
+		System.out.println("integer value is " + a);
+		System.out.println("long value is : " + l);
+		System.out.println("float value is : " + f);
+		System.out.println("double value is : " + d);
+		System.out.println("boolean value is : " + bFlag);
+		System.out.println("char value is : " + c);
+			
+	}
+
+}
