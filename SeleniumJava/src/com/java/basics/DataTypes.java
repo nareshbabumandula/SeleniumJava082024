2558edf08aa5ccfa8f64a0a749a393a78a45ff0b
nareshbabumandula <naresh223@gmail.com>
2024-07-31 07:04:02 +0530

    String example

--- SeleniumJava/src/com/java/basics/DataTypes.java
+++ SeleniumJava/src/com/java/basics/DataTypes.java
@@ -35,6 +35,7 @@ public class DataTypes {
 		byte b2=(byte)s2; // Explicit type casting or narrowing
 		System.out.println(s2 + " " +b2);
 		
+		// String is a pre-defined class in Java
 		String sText="welcome to java software edition";
 		String[] arrWords = sText.split(" ");
 		System.out.println(arrWords[0]);
