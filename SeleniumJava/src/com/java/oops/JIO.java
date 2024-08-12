1353bb4ed369df08f2222351a8659be4b00f2137
nareshbabumandula <naresh223@gmail.com>
2024-08-12 08:07:52 +0530

    12-Aug-2024: Naresh: abstract class and Interface in OOPS

---
+++ SeleniumJava/src/com/java/oops/JIO.java
@@ -0,0 +1,15 @@
+package com.java.oops;
+
+public class JIO implements SIM{
+
+	public static void main(String[] args) {
+		JIO jio = new JIO();
+		jio.SIMNumber();
+	}
+
+	@Override
+	public void SIMNumber() {
+	  System.out.println("SIM number is printed on every JIO sim");
+	}
+
+}
--- SeleniumJava/src/com/java/oops/RBI.java
+++ SeleniumJava/src/com/java/oops/RBI.java
@@ -4,7 +4,11 @@ public abstract class RBI {
 	
 	/**
 	 * Abstraction : Showing only the functionality to the end user by hiding it internal details
-	 * e.g. messaging, whatsapp call, facebook posts, bluetooth etc
+	 * e.g. messaging, calling feature, whatsapp call, facebook posts, bluetooth etc
+	 * 1. An abstract class can have abstract methods as well as non abstract methods
+	 * 2. To achieve 0 to 100% abstraction we can go for abstract class
+	 * 3. We cannot create an object reference for an abstract class because abstract class has partial implementation
+	 * 4. We can't instantiate an abstract class directly because it might contain abstract methods that lack implementation
 	 */
 	// abstract method or unimplemented methods or bodyless methods
 	abstract void loans();
@@ -12,7 +16,7 @@ public abstract class RBI {
 	abstract void accountClosing();
 	abstract void KYC();
 		
-	// User defined abstract method or concrete java method 
+	// User defined non-abstract method or concrete java method or method with body
 	void creditcard() {
 		// method body or code implementation
 		System.out.println("Creditcard facility may or may not be there.!");
@@ -20,7 +24,7 @@ public abstract class RBI {
 	
 
 	public static void main(String[] args) {
-		
+		// RBI rbi = new RBI(); // Cannot instantiate the type RBI
 	}
 
 }
