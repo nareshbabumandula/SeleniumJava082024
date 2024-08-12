d856be27d0c2a526d23273fb6e8fc965ba40304a
Preethi <Preethi@DESKTOP-D5NGQE7>
2024-08-12 23:03:16 +0530

    12-08-2024 Class Programs on Abstract Class and Interface

---
+++ SeleniumJava/src/com/java/oops/RBI.java
@@ -0,0 +1,30 @@
+package com.java.oops;
+
+public abstract class RBI {
+	
+	/**
+	 * Abstraction : Showing only the functionality to the end user by hiding it internal details
+	 * e.g. messaging, calling feature, whatsapp call, facebook posts, bluetooth etc
+	 * 1. An abstract class can have abstract methods as well as non abstract methods
+	 * 2. To achieve 0 to 100% abstraction we can go for abstract class
+	 * 3. We cannot create an object reference for an abstract class because abstract class has partial implementation
+	 * 4. We can't instantiate an abstract class directly because it might contain abstract methods that lack implementation
+	 */
+	// abstract method or unimplemented methods or bodyless methods
+	abstract void loans();
+	abstract void accountOpening();
+	abstract void accountClosing();
+	abstract void KYC();
+		
+	// User defined non-abstract method or concrete java method or method with body
+	void creditcard() {
+		// method body or code implementation
+		System.out.println("Creditcard facility may or may not be there.!");
+	}
+	
+
+	public static void main(String[] args) {
+		// RBI rbi = new RBI(); // Cannot instantiate the type RBI
+	}
+
+}
---
+++ SeleniumJava/src/com/java/oops/SIM.java
@@ -0,0 +1,37 @@
+package com.java.oops;
+
+/**
+ * interface: To achieve 100% abstraction we go for interface
+ * 1. Interface will not allow non-abstract concrete methods(method with body)
+ * 2. By default all the methods and variables are abstract and final in an interface
+ * 3. 4. We can't instantiate an interface because it only defines method signatures, not the implementation
+ * 
+ * Note: In object oriented programming, an abstract class or an interface is a blueprint/contract/template that
+ * cannot be instantiated directly. The reason for this is that these constructs are designed to define
+ * a common structure or contract without providing a complete implementation.
+ *  
+ * @author nares
+ *
+ */
+public interface SIM {
+	
+	String size = "0.75mm";
+	String model="";
+	int price=100;
+	
+	void SIMNumber();
+	
+	static void getManufacturer(String manufacturer) {
+		System.out.println("Manufacturer is : " + manufacturer);
+	}
+	
+	default void getSIMType(String simType) {
+		System.out.println("SIM type is : " + simType);
+	}
+	
+	public static void main(String[] args) {
+		//SIM s = new SIM();  // Cannot instantiate the type SIM
+	}
+	
+
+}
