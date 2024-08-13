a9450ed6dcd684f66ef1cacc055db4151aec9695
Preethi <Preethi@DESKTOP-D5NGQE7>
2024-08-13 08:00:40 +0530

    Test Commit

---
+++ SeleniumJava/src/com/oops/practice/Laptop.java
@@ -0,0 +1,17 @@
+package com.oops.practice;
+
+public abstract class Laptop {
+
+	abstract void CPU();
+	abstract void RAM();
+	abstract void Batterylife();
+	void mouse()
+	{
+		System.out.println("Bluetooth mouse is included");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
