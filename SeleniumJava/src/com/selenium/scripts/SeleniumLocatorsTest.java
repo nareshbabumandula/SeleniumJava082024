76595359cb8b11c7eeb72e3dbd5a11b3dcb4c93f
nareshbabumandula <naresh223@gmail.com>
2024-09-20 07:59:11 +0530

    20-Sep-2024: Naresh: Selenium locators basic

--- SeleniumJava/src/com/selenium/scripts/SeleniumLocatorsTest.java
+++ SeleniumJava/src/com/selenium/scripts/SeleniumLocatorsTest.java
@@ -3,6 +3,9 @@ package com.selenium.scripts;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 public class SeleniumLocatorsTest {
 
@@ -12,15 +15,48 @@ public class SeleniumLocatorsTest {
 		driver = new ChromeDriver();
 		driver.get("https://www.mycontactform.com");
 		driver.manage().window().maximize();
-		driver.findElement(By.linkText("Sample Forms")).click();
+
 		// id locator
-		driver.findElement(By.id("subject")).sendKeys("Test Subject111");
-		Thread.sleep(4000);
-		driver.findElement(By.id("subject")).clear();
+		driver.findElement(By.id("user")).sendKeys("Seshu");
+		Thread.sleep(2000);
+		driver.findElement(By.id("user")).clear();
+
 		// name locator
-		driver.findElement(By.name("subject")).sendKeys("Test Subject222");
-		Thread.sleep(4000);
+		driver.findElement(By.name("user")).sendKeys("Sajjad");
+		Thread.sleep(2000);
+		driver.findElement(By.name("user")).clear();
+
+		// class name locator
+		driver.findElement(By.className("txt_log")).sendKeys("Gouthami");
+		Thread.sleep(2000);
+		driver.findElement(By.className("txt_log")).clear();
+
+		// css locator --> syntax html tag[attribute='value']
+		driver.findElement(By.cssSelector("input[id='user']")).sendKeys("Preethi");
+		Thread.sleep(2000);
+		driver.findElement(By.cssSelector("input[id='user']")).clear();
+
+		// xpath locator --> syntax //html tag[@attribute='value']
+		driver.findElement(By.xpath("//input[@id='user']")).sendKeys("Noor");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@id='user']")).clear();
+
+		// tagName locator
+		driver.findElement(By.tagName("input")).sendKeys("Naresh");
+		Thread.sleep(2000);
+		driver.findElement(By.tagName("input")).clear();
+
+		// linktext locator
+		driver.findElement(By.linkText(">> More Testimonials")).click();
+		Thread.sleep(2000);
+
+		driver.navigate().to("https://www.mycontactform.com");
+		Thread.sleep(5000);
+
+		// partial linktext locator
+		driver.findElement(By.partialLinkText("e Testimonials")).click();
+		Thread.sleep(2000);
+
 		driver.quit();
 	}
-
 }
