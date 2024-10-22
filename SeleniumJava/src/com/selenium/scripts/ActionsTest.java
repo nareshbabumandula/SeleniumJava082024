6af2bb6fc9a56341bbc4528faf13e3528ba5ef40
[nareshbabumandula] <naresh223@gmail.com>
2024-10-22 07:18:24 +0530

    22-Oct-2024: Naresh: Working with mouse over actions and testNG listeners

---
+++ SeleniumJava/src/com/selenium/scripts/ActionsTest.java
@@ -0,0 +1,53 @@
+package com.selenium.scripts;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import com.listeners.CustomListener;
+
+@Listeners(CustomListener.class)
+public class ActionsTest {
+
+	static WebDriver driver;
+
+	@BeforeClass
+	public void accessSite() {
+		ChromeOptions ops = new ChromeOptions();
+		ops.addArguments("--disable-notifications");
+		driver = new ChromeDriver(ops);
+		driver.get("https://www.spicejet.com/");
+		driver.manage().window().maximize();
+	}
+
+	@AfterClass
+	public void closeBrowser() {
+		driver.quit();
+	}
+
+	@Test
+	public void actionMethods() throws InterruptedException {
+		Assert.assertEquals(false, true);
+		// Working with mouse over actions
+		WebElement addons = driver.findElement(By.xpath("//div[contains(text(),'Add-ons123')]"));
+		Actions action = new Actions(driver);
+		action.moveToElement(addons).perform();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+		//wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//div[contains(text(),'Taxi')])[1]"))).click();
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@data-testid='test-id-Taxi']/descendant::div[contains(text(),'Taxi')]"))).click();
+		
+		Thread.sleep(5000);
+	}
+
+}
