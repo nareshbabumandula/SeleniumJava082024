651791d387458c7337de4475a50fba63778dfdbd
Preethi <Preethi@DESKTOP-D5NGQE7>
2024-10-24 06:42:39 +0530

    Practiced Interface

--- SeleniumJava/src/com/java/oops/InorbitM.java
+++ SeleniumJava/src/com/java/oops/InorbitM.java
@@ -1,6 +1,6 @@
 package com.java.oops;
 
-public class InorbitM extends Theatre
+public class InorbitM implements Theatre
 {
 
 	public static void main(String[] args) {
@@ -10,7 +10,14 @@ inorbitM.telugushows();
 inorbitM.hindishows();
 inorbitM.agelimit();
 inorbitM.screentypes();
-		
+inorbitM.carParking(100);
+inorbitM.bikeParking(150);
+inorbitM.soundSystem(true);
+inorbitM.emergencyExit(5);
+inorbitM.canteen(20);
+inorbitM.fireandSafety("entire buiding");
+inorbitM.displayTheatreName("PVR");
+
 	}
 void telugushows() 
 {
@@ -27,5 +34,40 @@ void agelimit()
 void screentypes()
 {
 	System.out.println("2D,3D");
+}
+@Override
+public void carParking(int numberOfCars) {
+	System.out.println("Number of cars parked: " + numberOfCars);
+	
+}
+@Override
+public void bikeParking(int numberOfBikes) {
+	System.out.println("Number of bikes parked: " + numberOfBikes);
+	
+}
+@Override
+public void soundSystem(boolean isSoundSytemOn) {
+	if (isSoundSytemOn) {
+		System.out.println("The sound system is ON");
+	} else {
+		System.out.println("The sound system is OFF");
+	}
+}
+@Override
+public void emergencyExit(int numberOfExits) {
+	System.out.println("Number of emergency exits available : "+ numberOfExits);
+}
+@Override
+public void fireandSafety(String safetyProtocol) {
+	System.out.println("Fire and Safety protocol being followed for the : " + safetyProtocol);
+}
+@Override
+public void canteen(int availbleItems) {
+	System.out.println("Number of items available in the canteen : " +availbleItems);
+}
+@Override
+public void seatingCategories(String categoryType, int numberOfSeats) {
+	System.out.println("Seating Category : " + categoryType + "| number of seats avaiable : " + numberOfSeats);
+	
 }
 }
